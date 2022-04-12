1bc97156485fa62136ee53da03fe157fb5235d15
Oscar Sangrador <oscarsangrador@gmail.com>
2022-04-12 18:34:05 +0200

    up server and basic users routes

---
+++ config/routes.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+
+
+const usersController = require('../controllers/users.controller');
+const authController = require('../controllers/auth.controller');
+
+
+router.get('/', (req, res, next) => {
+
+  console.log('hola');
+
+  res.status(200).json({ ok: true })
+
+});
+
+/* Auth */
+
+router.post('/login', authController.login);
+
+
+
+
+/* Users */
+
+
+
+
+router.post('/users', authController.create);
+router.get('/users/:id', usersController.getUserById);
+
+
+
+
+module.exports = router;
